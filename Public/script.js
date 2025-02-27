b62b6ff621ff6eae315b929e6c82e9ddb7685594
Joaquin Colman <colmanjoaco.it@gmail.com>
2025-02-27 05:30:05 +0100

    corregi el fetch

--- Public/script.js
+++ Public/script.js
@@ -18,7 +18,7 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     const formMessage = document.getElementById('form-message');
 
     if (name && email && subject && message) {
-        fetch('http://localhost:4000/api/contact', {
+        fetch('/api/contact', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ name, email, subject, message })
